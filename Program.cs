d79aa0eae7f2a2670b7c0fd9855d8543f1ad3eab
Augusto-02 <augustonodari@gmail.com>
2024-08-25 18:25:43 -0300

    Feature/Main
    Foi criado o projeto em c# dotnet, e foram criados a funcionalidade main do programa, além da validação dos inputs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+class Program
+{
+      static void Main(){
+          var user =  getUserInformation();
+          Console.WriteLine(user.buyValue);
+          
+      }
+
+      static (string active, int buyValue, int sellValue ) getUserInformation() {
+            string active =""; 
+            int buyValue = 0;
+            int sellValue = 0;
+            try
+            {
+                  Console.Write("Digite o nome da ação: ");
+                  active = Console.ReadLine();
+
+                  Console.Write("Digite o valor de compra da ação: ");
+                  buyValue = int.Parse(Console.ReadLine());
+
+                  Console.Write("Digite o valor de venda da ação: ");
+                  sellValue = int.Parse(Console.ReadLine());
+
+                  return (active, buyValue, sellValue);
+            }
+            catch (System.Exception exception)
+            {
+                  Console.WriteLine(" ");
+                  Console.WriteLine($"Ocorreu um erro no programa {exception.Message} Por favor verifique se você digitou os parametros corretos!");
+            }
+            
+            return (active, buyValue, sellValue);
+            
+            }
+}
+
+
+
